16d1be38ef64553b200ec12bb3e41ecadfe41459
IgorSandes <igorsandes213@gmail.com>
2024-01-08 01:06:21 -0300

    adicionando alterações feitas pelo time

--- js/buscar.js
+++ js/buscar.js
@@ -1,11 +1,12 @@
 function pesquisar(){
 
-    const busca = document.getElementById("buscar").value.toLowerCase()
-    let livro = [...document.getElementsByClassName('info-livros')]
+    const busca = document.getElementById("buscar").value.toLowerCase();
+    let livro = [...document.getElementsByClassName('info-livros')];
+
     console.log(busca)
     for(i = 0; i < livro.length; i++){
         if(!livro[i].textContent.toLowerCase().includes(busca)){
-            livro[i].style.display = "none";       
+            livro[i].style.display = "none";
         }
         else{
             // console.log(livro[i])
